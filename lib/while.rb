c667d3efb36c7ada78e1813e4effe327dfb83912
Sonya Gould <sonyavgould@gmail.com>
2020-05-25 23:02:12 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,8 @@
   def using_while
     levitation_force = 6
-    counter = 0
   while levitation_force < 10
     puts "Wingardium Leviosa"
-    counter += 1
-    levitation_force += counter
+    levitation_force += 1
   end
 end
 
